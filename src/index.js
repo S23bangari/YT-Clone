51acbf65183a7958f0f52507e503e9638d7577de
S23bangari <somilbangari@gmail.com>
2024-09-06 01:30:03 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // require('dotenv').config()
+import { app } from './app.js';
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 dotenv.config({
@@ -41,3 +42,6 @@ connectDB()
         
 //     }
 // } )()
+
+
+
